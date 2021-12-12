6702ef975ea5cc6cd7fa74e6f9d3625afeff8b7e
mk812y <mk812y@gmail.com>
2021-12-12 17:09:01 +0300

    Setup View + Stack Slider

--- HomeWork22/ViewController.swift
+++ HomeWork22/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainView.backgroundColor = .white
+        view.backgroundColor = .blue
     }
 
 
