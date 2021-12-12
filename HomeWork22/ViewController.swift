87d4c92794ea29ff2f461c5437875e454a43be53
mk812y <mk812y@gmail.com>
2021-12-12 17:22:50 +0300

    Setup Slider v1.0

--- HomeWork22/ViewController.swift
+++ HomeWork22/ViewController.swift
@@ -11,11 +11,43 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainView: UIView!
     
+    @IBOutlet weak var labelRedSliderName: UILabel!
+    @IBOutlet weak var labelGreenSliderName: UILabel!
+    @IBOutlet weak var labelBlueSliderName: UILabel!
+    
+    @IBOutlet weak var labelRedSliderValue: UILabel!
+    @IBOutlet weak var labelGreenSliderValue: UILabel!
+    @IBOutlet weak var labelBlueSliderValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mainView.backgroundColor = .white
         view.backgroundColor = .blue
+        
+        labelRedSliderName.text = "Red"
+        labelRedSliderName.textColor = .red
+        labelGreenSliderName.text = "Green"
+        labelGreenSliderName.textColor = .green
+        labelBlueSliderName.text = "Blue"
+        labelBlueSliderName.textColor = .blue
+        
+        redSlider.value = 1
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 100
+        greenSlider.value = 1
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 100
+        blueSlider.value = 1
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 100
+
+        
     }
 
 
