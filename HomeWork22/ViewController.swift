ae70705cf0200bcca43b080cb219222eae78c8e0
mk812y <mk812y@gmail.com>
2021-12-12 18:54:17 +0300

    Setup v2

--- HomeWork22/ViewController.swift
+++ HomeWork22/ViewController.swift
@@ -23,13 +23,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    private let minimumTrackTintColor = UIColor.cyan
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mainView.backgroundColor = .white
         mainView.layer.cornerRadius = 10
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBlue
         
         labelRedSliderName.text = "Red"
         labelRedSliderName.textColor = .white
@@ -50,20 +50,43 @@ class ViewController: UIViewController {
         
         redSlider.value = 1
         redSlider.minimumValue = 0
-        redSlider.maximumValue = 100
+        redSlider.maximumValue = 255
         redSlider.thumbTintColor = .red
+        redSlider.minimumTrackTintColor = minimumTrackTintColor
         greenSlider.value = 1
         greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 100
+        greenSlider.maximumValue = 255
         greenSlider.thumbTintColor = .green
         blueSlider.value = 1
         blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 100
+        blueSlider.maximumValue = 255
         blueSlider.thumbTintColor = .blue
-
         
+        mainView.backgroundColor = UIColor(red: 1/255, green: 1/255, blue: 1/255, alpha: 1)
+
     }
 
-
+    @IBAction func actionSliderRed(_ sender: UISlider) {
+        let currencyGreenSliderValue = Double(round(100 * CGFloat(greenSlider.value)) / 100)
+        let currencyBlueSliderValue = Double(round(100 * CGFloat(blueSlider.value)) / 100)
+        let redSliderValue = Double(round(100 * CGFloat(redSlider.value)) / 100)
+        mainView.backgroundColor = UIColor(red: redSliderValue/255, green: currencyGreenSliderValue/255, blue: currencyBlueSliderValue/255, alpha: 1)
+        labelRedSliderValue.text = "\(redSliderValue)"
+    }
+    @IBAction func actionSliderGreen(_ sender: UISlider) {
+        let currencyRedSliderValue = Double(round(100 * CGFloat(redSlider.value)) / 100)
+        let currencyBlueSliderValue = Double(round(100 * CGFloat(blueSlider.value)) / 100)
+        let greenSliderValue = Double(round(100 * CGFloat(greenSlider.value)) / 100)
+        mainView.backgroundColor = UIColor(red: currencyRedSliderValue/255, green: greenSliderValue/255, blue: currencyBlueSliderValue/255, alpha: 1)
+        labelGreenSliderValue.text = "\(greenSliderValue)"
+    }
+    @IBAction func actionSliderBlue(_ sender: UISlider) {
+        let currencyRedSliderValue = Double(round(100 * CGFloat(redSlider.value)) / 100)
+        let currencyGreenSliderValue = Double(round(100 * CGFloat(greenSlider.value)) / 100)
+        let blueSliderValue = Double(round(100 * CGFloat(blueSlider.value)) / 100)
+        mainView.backgroundColor = UIColor(red: currencyRedSliderValue/255, green: currencyGreenSliderValue/255, blue: blueSliderValue/255, alpha: 1)
+        labelBlueSliderValue.text = "\(blueSliderValue)"
+    }
+    
 }
 
